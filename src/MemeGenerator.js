d3b0515722a74dab8b12db674e04ee1ec03f824b
Kelly Coles <kellycoles1@gmail.com>
2020-03-21 19:45:12 -0500

    made header component and MemeGenerator class

---
+++ src/MemeGenerator.js
@@ -0,0 +1,15 @@
+import React, {Component} from "react"
+
+class MemeGenerator extends Component {
+    constructor() {
+        super()
+        this.state = {}
+    }
+    render(){
+        return (
+            <h1>Meme Generator Section</h1>
+        )
+    }
+}
+
+export default MemeGenerator
